53953262022db1f34548334b30dbda947a7d77e8
Rajapandian C <rajapandianc@outlook.in>
2020-09-18 06:33:51 +0530

    Secured projects using Spring Security [HTTP Basic Authentication]

---
+++ src/main/java/com/crpdev/mssc/oil/inventory/service/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.crpdev.mssc.oil.inventory.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by rajapandian
+ * Date: 18/09/20
+ * Project: mssc-oil-eureka
+ * Package: com.crpdev.mssc.oil.inventory.service.config
+ **/
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
